f2eb5af74790757ae45d835fb7913b257761f6d4
usmanibraheem <usmanibraheem74@gmail.com>
2025-06-23 18:36:21 +0500

    certbot install

--- install-certbot.sh
+++ install-certbot.sh
@@ -1,6 +1,2 @@
 echo "Installing Certbot..."
-apt isntall snapd
-snap install core
-snap refresh core
-snap install --classic certbot
-ln -s /snap/bin/certbot /usr/bin/certbot
+apt install certbot python3-certbot-nginx
