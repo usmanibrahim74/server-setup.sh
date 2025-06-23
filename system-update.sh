23aaef05bebce20ca8a80072969b7ceb1e88b1bd
usmanibraheem <usmanibraheem74@gmail.com>
2025-06-23 18:23:29 +0500

    system update

--- system-update.sh
+++ system-update.sh
@@ -1,6 +1,5 @@
 # ====================== SYSTEM UPDATE & CLEANUP ======================
    echo "Updating system and removing unnecessary packages..."
   apt-get update -qq
-  apt-get purge -y apache2* snapd
+  apt-get purge -y apache2*
   apt-get autoremove -y --purge
-  apt-get upgrade -y
