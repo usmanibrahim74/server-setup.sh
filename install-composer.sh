5858fc72f0c68e5315f1c7fc121a58efd672e4ce
usmanibraheem <usmanibraheem74@gmail.com>
2025-06-23 18:48:30 +0500

    install composer check

--- install-composer.sh
+++ install-composer.sh
@@ -17,7 +17,7 @@ if command -v composer &> /dev/null; then
 fi
 
 echo "Installing Composer..."
-  EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php");')"
+  EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
   php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
   ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
